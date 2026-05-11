682868e5bc36a1fa0544ce004f88003f1c172c91
CNISIDIS <makeithappen@nisidis.com>
2026-05-11 17:13:45 +0300

    BREAKING CHANGES - Update to latest vvvv stable 7.1 , net8 (remove language def) rework on TrackablePoint.cs (simplify)

---
+++ src/DBSCAN.Utils/Convertor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using VL.Lib.Mathematics;
+using Dbscan;
+
+namespace DBSCAN.Utils
+{
+    /// <summary>
+    /// Class implementing IPointData to use VL's Vector2 with DBSCAN plugin
+    /// </summary>
+    public static class Convertor
+    {
+
+
+        
+        public static Dbscan.Point FromVector(Stride.Core.Mathematics.Vector2 Point)
+        {
+            return new Dbscan.Point(Point.X, Point.Y);
+        }
+
+        
+
+        
+        public static Stride.Core.Mathematics.Vector2 ToVector2(Dbscan.Point point)
+        {
+            return new Stride.Core.Mathematics.Vector2((float)point.X, (float)point.Y);
+        }
+    }
+}
---
+++ src/DBSCAN.Utils/TrackablePoint.cs
@@ -0,0 +1,25 @@
+﻿using Dbscan;
+using Stride.Core.Mathematics;
+using System;
+using VL.Core;
+
+namespace DBSCAN.Utils
+{
+    public record TrackablePoint : IPointData
+    {
+        readonly private Dbscan.Point _point;
+        public Dbscan.Point Point => _point;
+
+        public Stride.Core.Mathematics.Vector2 Position => new Vector2((float)_point.X, (float)_point.Y);
+
+        public TrackablePoint(Stride.Core.Mathematics.Vector2 position)
+        {
+            _point = new Dbscan.Point(position.X, position.Y);
+        }
+        
+
+
+    }
+
+    
+}
